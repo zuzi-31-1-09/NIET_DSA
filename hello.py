4d40c32ea395c264947d577d58a6c52866f609d0
zuzi-31-1-09 <your_github_krmananya5952@gmail.com>
2026-05-26 13:26:03 +0530

    Optimized Two Sum

--- hello.py
+++ hello.py
@@ -1,19 +1,21 @@
-# Linear Search function
-def linear_search(arr, target):
-    # Loop through every index and item in the array
-    for index in range(len(arr)):
-        if arr[index] == target:
-            return index  # Target found! Return its position.
+def two_sum_optimized(nums, target):
+    seen_numbers = {} # This is our Hash Map (Value : Index)
+    
+    for current_index, current_num in enumerate(nums):
+        complement = target - current_num
+        
+        # Check if the needed number is already in our map
+        if complement in seen_numbers:
+            return [seen_numbers[complement], current_index]
             
-    return -1  # Target not found in the entire list
+        # If not found, store the current number and its position
+        seen_numbers[current_num] = current_index
+        
+    return []
 
-# Let's test it with an array of numbers
-my_list = [10, 23, 45, 70, 11, 15]
+# Test with the same numbers
+numbers = [2, 7, 11, 15]
+target_sum = 9
 
-# Test 1: Finding an item that exists
-result_1 = linear_search(my_list, 70)
-print(f"Target 70 found at index position: {result_1}")
-
-# Test 2: Finding an item that does NOT exist
-result_2 = linear_search(my_list, 99)
-print(f"Target 99 found at index position: {result_2}")
+result = two_sum_optimized(numbers, target_sum)
+print(f"Optimized search found indices: {result}")
