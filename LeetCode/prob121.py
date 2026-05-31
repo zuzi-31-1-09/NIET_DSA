4fa9bb585bbd5b3931b95784a6d35deb9656d72a
zuzi-31-1-09 <krmananya5952@gmail.com>
2026-05-31 13:20:06 +0530

    feat: complete leetcode 121 best time to buy and sell stock

---
+++ LeetCode/prob121.py
@@ -0,0 +1,15 @@
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        # Initialize your lowest buying price to a very high number
+        min_price = float('inf')
+        max_profit = 0
+        
+        for price in prices:
+            # Update min_price if we find a cheaper day to buy
+            if price < min_price:
+                min_price = price
+            # Otherwise, calculate profit if sold today and check if it's our max
+            elif price - min_price > max_profit:
+                max_profit = price - min_price
+                
+        return max_profit
